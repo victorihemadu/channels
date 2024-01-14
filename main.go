e6714212ca26c4ba32d6169aa08f5610e63253ae
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2024-01-14 17:44:56 +0100

    channels

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	links := []string{
+		"http://google.co,",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+}
